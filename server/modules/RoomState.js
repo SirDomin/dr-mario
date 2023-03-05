fbc05f2b10116897d32814cea0fdd8e412ef5d7f
Rafikooo <thecrazyrafik@gmail.com>
2023-03-05 05:31:45 +0100

    [Server] Add RoomState

---
+++ server/modules/RoomState.js
@@ -0,0 +1,7 @@
+export const RoomState = {
+    EMPTY: 'Empty',
+    WAITING_FOR_OPPONENT: 'Waiting for the opponent',
+    READY: 'Ready to start',
+    PLAYING: 'Playing',
+    GAME_OVER: 'Game over',
+}
