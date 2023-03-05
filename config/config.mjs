f703d27f6b22641253fcf04f39f6ce2d5ee96c98
Rafikooo <thecrazyrafik@gmail.com>
2023-03-05 21:19:36 +0100

    [Test] Change protocol from ws to wss

--- config/config.mjs
+++ config/config.mjs
@@ -1,6 +1,6 @@
 export const Config = {
     port: 443,
     // address: 'wss://mario.livestockvet.pl:443',
-    address: 'ws://206.81.26.214:443',
+    address: 'wss://206.81.26.214:443',
     // address: 'ws://192.168.0.61:81',
 };
